969ccad39a342e12f286eada5a7de2b129af4a2c
Kim Seungha <seungha.me@gmail.com>
2018-01-30 16:18:29 +0900

    로그인 기능 완성

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css'
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyC5bvpoKyfa3qwTxhSt0PxgQZI2dI3QbZc",
+  authDomain: "fds-cra.firebaseapp.com",
+  databaseURL: "https://fds-cra.firebaseio.com",
+  projectId: "fds-cra",
+  storageBucket: "fds-cra.appspot.com",
+  messagingSenderId: "966283711333"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
